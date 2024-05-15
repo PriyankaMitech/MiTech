e5dbcd96cd7ba1ab46a84d820dc4ad8c5af1cd48
rupali21 <rupali21.mitech@gmail.com>
2024-05-15 16:22:00 +0530

    employeelist- download button

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -452,9 +452,6 @@ $PageName = $this->request->getPost('PageName');
 // $Description = $this->request->getPost('Description');
 // $condition = $this->request->getPost('condition');
 $Taskradio = $this->request->getPost('Taskradio');
-
-
-
 // Instantiate your model
 $model = new Adminmodel();
 
