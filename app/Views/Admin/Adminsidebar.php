cf7d6b740fb9fba07e7db4c580b6382278a39571
PriyankaMitech <priyanka.mitech@gmail.com>
2024-05-17 11:33:16 +0530

    Small Change On Adminsidebar

--- app/Views/Admin/Adminsidebar.php
+++ app/Views/Admin/Adminsidebar.php
@@ -415,7 +415,7 @@
                         </li>
                         <li class="nav-item">
                             <a href="#" class="nav-link">
-                                <i class="nav-icon fas fa-users"></i>
+                            <i class="nav-icon fas fa-user-tie"></i>
                                 <p>
                                     Client
                                     <i class="fas fa-angle-left right"></i>
