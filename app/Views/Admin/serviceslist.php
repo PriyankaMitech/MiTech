cec7c4b46b382cc66ac467ec9a4cb7f3f6d1fb40
PriyankaMitech <priyankag.mitech@gmail.com>
2024-05-17 11:20:53 +0530

    create new master add servicess and showing client name in dropdown

---
+++ app/Views/Admin/serviceslist.php
@@ -0,0 +1,67 @@
+<?php echo view('Admin/Adminsidebar.php'); ?>
+
+<div class="content-wrapper">
+
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="text-white">Service List</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active text-white">Service List</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Service List</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Sr.No</th>
+                                        <th>Service Name</th>
+                                        <th>Action</th>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php if (!empty($menu_data)) {
+                                        $i = 1; ?>
+                                        <?php foreach ($menu_data as $data) {  ?>
+                                            <tr>
+                                                <td><?= $i; ?></td>
+                                                <td><?= $data->ServicesName; ?></td>
+                                                <td>
+                                                    <a href="addservices/<?= $data->id; ?>"><i class="far fa-edit me-2"></i></a>
+                                                    <a href="<?= base_url(); ?>delete_compan/<?php echo base64_encode($data->id); ?>/tbl_services" onclick="return confirm('Are You Sure You Want To Delete This Record?')"><i class="far fa-trash-alt me-2"></i></a>
+                                                </td>
+
+                                            </tr>
+                                        <?php $i++;
+                                        } ?>
+                                    <?php } ?>
+
+                                </tbody>
+
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<?php echo view('Admin/Adminfooter.php');?>      
